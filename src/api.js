568306abfa135a6a6929297004ad0586f34ada47
Gabriel Nardy <gacnardy@gmail.com>
2024-06-26 20:14:36 -0300

    tentando melhorar chamada api

--- src/api.js
+++ src/api.js
@@ -1,19 +1,20 @@
 import axios from 'axios';
 
-// URL base da API fornecida do git
 const API_URL = 'https://fake-api.tractian.com';
 
-// Função para obter todas as empresas listadas na api
+// Função para obter as empresas
 export const getCompanies = async () => {
-  return axios.get(`${API_URL}/companies`);
+  return await axios.get(`${API_URL}/companies`);
 };
 
-// Função para obter todas as localizações de uma empresa específica pelo id
+// Função para obter as localizações da empresa
 export const getCompanyLocations = async (companyId) => {
-  return axios.get(`${API_URL}/companies/${companyId}/locations`);
+  return await axios.get(`${API_URL}/companies/${companyId}/locations`);
 };
 
-// Função para obter todos os ativos de uma empresa específica pelo id
-export const getCompanyAssets = async (companyId) => {
-  return axios.get(`${API_URL}/companies/${companyId}/assets`);
+// Função para obter os ativos da empresa com paginação
+export const getCompanyAssetsPaginated = async (companyId, page, pageSize = 10) => {
+  return await axios.get(`${API_URL}/companies/${companyId}/assets`, {
+    params: { page, pageSize }
+  });
 };
